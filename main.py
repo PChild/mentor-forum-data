1b2d133fcd565ebf33270a5e1ff4057c00698601
Preston <childrep@vt.edu>
2022-05-21 19:36:12 -0400

    Processing code for saved JSON files.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from ratelimit import sleep_and_retry
 import csv
 import json
 import enlighten
+import os
 
-manager = enlighten.get_manager()
 
 @sleep_and_retry
 @ratelimit.limits(calls=3, period=1)
@@ -26,17 +26,50 @@ def fetch_user_files(users_csv='mf_users.csv'):
         for row in csv.reader(infile):
             mf_users.append(row[0])
 
-    user_counter = manager.counter(total=len(mf_users), desc="Users".rjust(12), unit="Users", color="blue", leave=False)
+    user_counter = enlighten.Counter(total=len(mf_users), desc="Users", unit="Users", color="blue", leave=False)
     for user in mf_users:
         fetch_user_details(user)
         user_counter.update()
-    manager.stop()
+    user_counter.close()
 
 
-def process_mf_files():
-    print('hi')
+def process_mf_files(dir='mf_users/'):
+    mf_data = []
+    file_cnt = enlighten.Counter(total=len(os.listdir(dir)), desc="Users", unit="Users", color="blue", leave=False)
+    for file in os.listdir(dir):
+        try:
+            user = json.load(open(dir + file))['user']
+            if 'profile_hidden' in user.keys():
+                mf_data.append({
+                    'username': user['username'],
+                    'name': user['name'],
+                    'hidden': True
+                })
+            else:
+                mf_data.append({
+                    'username': user['username'],
+                    'name': user['name'],
+                    'hidden': False,
+                    'team': user['user_fields']['1'],
+                    'location': user['location'] if 'location' in user.keys() else '',
+                    'rookie_year': user['user_fields']['2'],
+                    'created': user['created_at'],
+                    'seen': user['last_seen_at'],
+                    'posted': user['last_posted_at'],
+                    'level': user['trust_level'],
+                    'reading_time': user['time_read'],
+                    'profile_views': user['profile_view_count']
+                })
+        except:
+            print("Problem:", file)
+        file_cnt.update()
+
+    with open('mf_data.csv', 'w', newline='') as outfile:
+        dict_writer = csv.DictWriter(outfile, mf_data[0].keys())
+        dict_writer.writeheader()
+        dict_writer.writerows(mf_data)
 
 
 if __name__ == '__main__':
-    print('hi')
+    process_mf_files()
 
